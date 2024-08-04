f934f789ce3c2ea3fed28a5097cc68fce6edbe27
Jacobo Alfaro <jacobo.alfaroh@autonoma.edu.co>
2024-08-03 21:54:22 -0500

    Control autorizacion de las rutas

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,22 @@ use App\Http\Controllers\Api\V1\AuthController;
 // })->middleware('auth:sanctum');
 
 Route::post('/v1/login', [AuthController::class,'login'])->name('api.login');
+//Category
 Route::get('/v1/categories', [CategoryController::class, 'index'])->name('categories.index');
+Route::get('/v1/categories/{category}', [CategoryController::class, 'show'])->name('categories.show');
+//Business
+Route::get('/v1/businesses', [BusinessController::class, 'index'])->name('businesses.index');
+Route::get('/v1/businesses/{business}', [BusinessController::class, 'show'])->name('businesses.show');
+//Location
+Route::get('/v1/locations', [LocationController::class, 'index'])->name('locations.index');
+Route::get('/v1/locations/{location}', [LocationController::class, 'show'])->name('locations.show');
+//Property
+Route::get('/v1/properties', [PropertyController::class, 'index'])->name('properties.index');
+Route::get('/v1/properties/{property}', [PropertyController::class, 'show'])->name('properties.show');
+//Characteristic
+Route::get('/v1/characteristics', [CharacteristicController::class, 'index'])->name('characteristics.index');
+Route::get('/v1/characteristics/{characteristic}', [CharacteristicController::class, 'show'])->name('characteristics.show');
+
 Route::apiResource('v1/propchars', CharacteristicPropertyController::class);
 
 
@@ -26,10 +41,9 @@ Route::apiResource('v1/propchars', CharacteristicPropertyController::class);
 Route::middleware(['auth:sanctum'])->group(function() {
 
     Route::post('/v1/logout', [AuthController::class, 'logout'])->name('api.logout');
-    Route::apiResource('v1/categories', CategoryController::class)
-        ->except(['index']);
-    Route::apiResource('v1/businesses', BusinessController::class);
-    Route::apiResource('v1/locations', LocationController::class);
-    Route::apiResource('v1/properties', PropertyController::class);
-    Route::apiResource('v1/characteristics', CharacteristicController::class);
+    Route::apiResource('v1/categories', CategoryController::class)->except(['index', 'show']);
+    Route::apiResource('v1/businesses', BusinessController::class)->except(['index', 'show']);
+    Route::apiResource('v1/locations', LocationController::class)->except(['index', 'show']);
+    Route::apiResource('v1/properties', PropertyController::class)->except(['index', 'show']);
+    Route::apiResource('v1/characteristics', CharacteristicController::class)->except(['index', 'show']);
  });
